2705b74a8c3f2e0e313492306a02737ffefb01db
Shuva <34592080+Shuvax@users.noreply.github.com>
2020-09-14 14:41:07 -0400

    Updates

---
+++ precache-manifest.1d58e4d6c85604e6e0447781f33a3844.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaeca42f7f05b7406f516770031d5baf",
+    "url": "/react-calc/index.html"
+  },
+  {
+    "revision": "4e12adebfbbbc0ee02b1",
+    "url": "/react-calc/static/css/main.45a3c6a8.chunk.css"
+  },
+  {
+    "revision": "6e82ec1521a7b03157cc",
+    "url": "/react-calc/static/js/2.74dc5623.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-calc/static/js/2.74dc5623.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e12adebfbbbc0ee02b1",
+    "url": "/react-calc/static/js/main.96bf4f72.chunk.js"
+  },
+  {
+    "revision": "a7259bb25855621b7ae5",
+    "url": "/react-calc/static/js/runtime-main.d9b56b08.js"
+  }
+]);
